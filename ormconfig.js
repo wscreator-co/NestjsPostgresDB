6d722980ba38b8573b5532fe8a70d92087dd4a57
Wordstyle Creator <52134979+wscreator-co@users.noreply.github.com>
2022-07-15 14:58:06 +0700

    v1.0.0

---
+++ ormconfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'type': 'postgres',
+	'url': process.env.DB_URL,
+	'entities': ['dist/**/*.entity{.ts,.js}'],
+	'synchronize': true,
+	'autoLoadEntities':true
+}
---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+	  TypeOrmModule.forRoot({
+      url: process.env.DATABASE_URL,
+      type: 'postgres',
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true, // This for development
+      autoLoadEntities: true,
+    }),
+	  UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
